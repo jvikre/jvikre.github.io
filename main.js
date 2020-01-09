5afff020684c4a5ad903c2cc0a147d5c1a928130
Julie Vikre <julievikre@gmail.com>
2020-01-09 16:32:46 -0500

    change background/projects/colors

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 $(document).ready(function () {
 	$('.menu-toggler').on('click', function () {
 		$(this).toggleClass('open');
@@ -22,4 +27,30 @@ $(document).ready(function () {
 		duration: 1800,
 		once: true
 	});
+
+	// Smooth Scroll Top
+	var sScrollTop = function () {
+
+		$(window).scroll(function(){
+			if ($(window).scrollTop() > 100 ) {
+				$('.up').show();
+			} else {
+				$('.up').hide();
+			}
+		});
+		$('.up').click(function(event){
+
+		    $('html, body').animate({
+		        scrollTop: 0
+		    }, 500);
+
+		    event.preventDefault();
+		    return false;
+		});
+	};
+
+	$(function(){
+		sScrollTop();
+	})
 });
+
